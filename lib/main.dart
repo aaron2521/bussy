3a523e5022b0e20745b5063e976e72eea710cf9f
aaron <aaronchellaya25@gmail.com>
2021-06-04 11:02:07 +0530

    splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bussy/main_page.dart';
+import 'package:bussy/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage()
+      home: SplashScreen()
     );
   }
 }
